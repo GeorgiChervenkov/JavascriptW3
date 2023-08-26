fc8d2e43ed918016e65462b66bd3a71bd2bfa2be
Georgi Chervenkov <georgi_chervenkov@abv.bg>
2023-08-26 22:21:26 +0300

    moved the two functions to external file

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function myfunction1() {document.getElementById("hehe").innerHTML = "the paragraph has changed when you clicked the butona dummy."}
+
+function myfunction2() {document.getElementById("hehe").innerHTML = "back to the original text."}
+
