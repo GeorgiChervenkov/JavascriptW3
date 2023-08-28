e87a714370307b0acc5017a398bba8dd03a42bb5
Georgi Chervenkov <georgi_chervenkov@abv.bg>
2023-08-28 18:42:00 +0300

    made some extra buttons

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
+let thing = "different"
+const first = "one", second = "two", third = "three"
 function myfunction1() {document.getElementById("hehe").innerHTML = "the paragraph has changed when you clicked the butona dummy."}
 
 function myfunction2() {document.getElementById("hehe").innerHTML = "back to the original text."}
 
+function myfunction3() {document.getElementById("gwent").innerHTML = thing;}
+
+function myfunction4() {document.getElementById("snap").innerHTML = second + " " + third + " " + first;}
